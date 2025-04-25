e7431794047815961363af366d760b88e1bd2731
greed <greed-d>
2025-04-25 13:18:33 +0545

    `init` : init project

---
+++ commerce/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class UserProfile(models.Model):
+    ...
+
+class BookID(models.Model):
+    ...
+
+class CustomerID(models.Model):
+    ...
+
+
+# Create your models here.
