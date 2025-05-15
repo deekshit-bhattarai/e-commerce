8c3e5685fc437ec6c0b28d8b6a068be5d4c7848d
greed <greed-d>
2025-05-15 11:19:22 +0545

    `feat(users)` : add user registration and login

---
+++ users/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.hashers import make_password
+from rest_framework import serializers
+
+from users.models import UserProfile
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={"input_type": "password"}, write_only=True)
+
+    class Meta:
+        model = UserProfile
+        fields = ["email", "password", "password2"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError("Passwords doesn't match")
+        if "password2" in attrs:
+            attrs.pop("password2")
+        return attrs
+
+    def create(self, validated_data):
+        breakpoint()
+        val_pass = validated_data.pop("password")
+        password = make_password(val_pass)
+        user = UserProfile.objects.create(
+            email=validated_data["email"], password=password
+        )
+        return user
