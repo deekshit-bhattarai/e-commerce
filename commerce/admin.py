bedc4b72e6569c024a1f0758b7cf137ac5929786
greed <greed-d>
2025-05-19 15:04:15 +0545

    `chore(inline)` : register model to admin panel and add inline where possible

--- commerce/admin.py
+++ commerce/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
+from .models import (
+    Cart,
+    CartItem,
+    Category,
+    Product,
+    ProductColor,
+    ProductSize,
+    ProductImage,
+    ProductVariant,
+    UserComment,
+    UserProfile,
+    UserReview,
+)
 
-# Register your models here.
+
+class ProductVariantInline(admin.TabularInline):
+    """
+    Tabular Inline for Product Variants.
+    Allows editing ProductVariant objects directly on the Product admin page.
+    Includes fields for Size and Color, as they are ForeignKeys on ProductVariant.
+    """
+
+    model = ProductVariant
+    extra = 1
+    fields = (
+        "size",
+        "color",
+        "price",
+        "stock",
+        "in_stock",
+    )
+
+
+class ProductCommentInline(admin.TabularInline):
+
+    model = UserComment
+    extra = 1
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductCommentInline,
+        ProductVariantInline,
+        ProductImageInline,
+    ]
+
+
+admin.site.register(
+    [Category, ProductColor, ProductSize, Cart, CartItem, UserComment, UserReview]
+)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(UserProfile)
