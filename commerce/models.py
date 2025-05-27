67e0b17ea23263e68b85a32f51263fc9b9ccd845
greed <greed-d>
2025-05-27 15:43:50 +0545

    `feat(contenttypes)` : add contenttypes framework for dynamic comments

--- commerce/models.py
+++ commerce/models.py
@@ -1,10 +1,10 @@
-from typing import override
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
 from django.db.models.signals import post_save
-import uuid
 from django.dispatch import receiver
 
-from django.conf import settings
-from django.db import models
 from product import models as product_app_model
 
 
@@ -22,19 +22,17 @@ class UserComment(models.Model):
     comment_user = models.ForeignKey(
         UserProfile, on_delete=models.CASCADE, related_name="comment_user"
     )
-    product = models.ForeignKey(
-        product_app_model.Product,
-        on_delete=models.CASCADE,
-        related_name="user_product_comment",
-    )
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveBigIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
 
     def __str__(self):
-        return f"{self.comment_user.user} | {self.product.name}"
+        return f"{self.comment_user.user} | {self.content_type}"
 
 
 class UserReview(models.Model):
     REVIEW_CHOICES = [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)]
-    stars = models.IntegerField(choices=REVIEW_CHOICES, max_length=10)
+    stars = models.IntegerField(choices=REVIEW_CHOICES)
     review_comment = models.CharField(max_length=600)
     review_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     review_created_at = models.DateTimeField(auto_now_add=True)
@@ -51,4 +49,5 @@ class UserReview(models.Model):
 @receiver(post_save, sender=UserProfile)
 def create_user_cart(sender, instance, created, **kwargs):
     if created:
+        from cart.models import Cart
         Cart.objects.create(user=instance)
