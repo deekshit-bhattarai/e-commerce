6860224af8923f7baad845eb37a43da3afd95e42
deekshit-bhattarai <bdeexeet@gmail.com>
2025-06-16 14:53:16 +0545

    `feat` : make sub-user children of merchant

--- merchant/permissions.py
+++ merchant/permissions.py
@@ -4,3 +4,10 @@ class IsMerchant(BasePermission):
     def has_permission(self, request, view):
         return request.user and request.user.is_authenticated and request.user.groups.filter(name="Merchant").exists() and request.user.is_merchant
 
+
+class IsMerchantOrSuperUser(BasePermission):
+    def has_permission(self, request, view):
+        is_authenticated = request.user and request.user.is_authenticated
+        is_merchant = request.user.groups.filter(name="Merchant").exists() and getattr(request.user, "is_merchant", False)
+        is_superuser = getattr(request.user, "is_superuser", False)
+        return is_authenticated and (is_merchant or is_superuser)
--- merchant/serializers.py
+++ merchant/serializers.py
@@ -5,19 +5,45 @@ from merchant.models import Merchant
 
 User = get_user_model()
 
-class MerchantSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Merchant
-        fields = "__all__"
 
 class SubUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
-        extra_kwargs = {
-            'password' : {'write_only' : True}
-        }
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        user = User.objects.create(**validated_data)
+        parent_user = self.context["request"].user
+
+        # Create the user
+        user = User.objects.create(**validated_data, parent=parent_user)
+
+        try:
+            parent_merchant = Merchant.objects.get(owner=parent_user)
+        except Merchant.DoesNotExist:
+            raise serializers.ValidationError(
+                "Merchant entry for the current user not found."
+            )
+
+        return user
+
+
+class MerchantSerializer(serializers.ModelSerializer):
+    owner = serializers.StringRelatedField()
+    subusers = SubUserSerializer(source="owner.sub_user", many=True, read_only=True)
+
+    class Meta:
+        model = Merchant
+        fields = [
+            "id",
+            "name",
+            "description",
+            "created_at",
+            "updated_at",
+            "owner",
+            "subusers",
+        ]
+
+    def create(self, validated_data):
+        user = Merchant.objects.create(**validated_data)
         return user
