4908082f6774b4aaac4abe2049203e3a6dd0ccec
deekshit-bhattarai <bdeexeet@gmail.com>
2025-06-16 14:53:25 +0545

    `chore` : formatting

--- product/admin/views.py
+++ product/admin/views.py
@@ -4,17 +4,17 @@ from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from product.models import ProductVariant
 from .serializers import ProductAdminSerializer
 
-class ProductVariantAdminView(ListModelMixin, CreateModelMixin, generics.GenericAPIView):
-    queryset = ProductVariant.objects.all()   
+
+class ProductVariantAdminView(
+    ListModelMixin, CreateModelMixin, generics.GenericAPIView
+):
+    queryset = ProductVariant.objects.all()
     serializer_class = ProductAdminSerializer
 
     def get(self, request):
         return self.list(request)
 
     def post(self, request, *args, **kwargs):
-        product = request.data 
+        product = request.data
         serializer = ProductAdminSerializer(product)
         serializer.save()
-
-        
-
