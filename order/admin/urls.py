d2c4b514e21c5168109e2fcd8ff9f38550d5ed96
deekshit-bhattarai <bdeexeet@gmail.com>
2025-06-17 15:11:40 +0545

    refac(urls) : refactor url for order

--- order/admin/urls.py
+++ order/admin/urls.py
@@ -1,11 +1,10 @@
-
 from rest_framework import routers
 
 from .views import AdminOrderViewSet
 
 router = routers.DefaultRouter()
 
-router.register(r"admin/orders", AdminOrderViewSet, basename="admin-orders")
+router.register(r"orders", AdminOrderViewSet, basename="admin-orders")
 
 urlpatterns = []
 urlpatterns += router.urls
