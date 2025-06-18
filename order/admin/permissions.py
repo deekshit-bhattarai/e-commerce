f09ef3c409c9496d47c05e9dda99582fa2abd96e
deekshit-bhattarai <bdeexeet@gmail.com>
2025-06-18 11:48:53 +0545

    feat : GET and POST method implemented

--- order/admin/permissions.py
+++ order/admin/permissions.py
@@ -1,9 +1,16 @@
 from rest_framework import permissions
 
+
 class IsAdminUser(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.is_staff 
+        return request.user and request.user.is_authenticated and request.user.is_staff
+
 
 class IsSuperAdminUser(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.is_staff and request.user.is_superadmin
+        return (
+            request.user
+            and request.user.is_authenticated
+            and request.user.is_staff
+            and request.user.is_superuser
+        )
--- order/admin/views.py
+++ order/admin/views.py
@@ -1,15 +1,43 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from django.contrib.auth import get_user_model
+from rest_framework.mixins import ListModelMixin, UpdateModelMixin
+from rest_framework.response import Response
 
 from order.models import Order
 from .serializers import AdminOrderSerializer
-from .permissions import IsAdminUser
+from .permissions import IsSuperAdminUser
 
 User = get_user_model()
 
 
-
-class AdminOrderViewSet(viewsets.ModelViewSet):
+class AdminOrderViewSet(ListModelMixin, UpdateModelMixin, viewsets.GenericViewSet):
     queryset = Order.objects.all()
     serializer_class = AdminOrderSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsSuperAdminUser]
+    lookup_field = "order_id"
+
+    def get(self, request, pk=None, *args, **kwargs):
+        if pk is not None:
+            try:
+                order = self.get_queryset().get(pk=pk)
+            except Order.DoesNotExist:
+                return Response(
+                    {"message": "Order doesn't exist"}, status=status.HTTP_404_NOT_FOUND
+                )
+
+            serializer = self.get_serializer(order)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            order = self.get_queryset()
+            serializer = self.get_serializer(order, many=True)
+            return Response(
+                {
+                    "success": True,
+                    "message": "Got all order successfully",
+                    "data": serializer.data,
+                },
+                status=status.HTTP_200_OK,
+            )
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
